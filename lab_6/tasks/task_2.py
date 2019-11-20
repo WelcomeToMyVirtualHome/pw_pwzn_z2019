340f0225d1c924d8c43b37ddf8b1c2720e965947
Jakub Sobolewski <jakupsob@gmail.com>
2019-11-20 21:55:26 +0100

    Task 2

--- lab_6/tasks/task_2.py
+++ lab_6/tasks/task_2.py
@@ -10,12 +10,25 @@ dwuelementową tuplę zawierającą liczbę poprawnych wierszy:
 - na indeksie 1 płeć M
 """
 import re
+import pandas as pd
 
+def match_msg(pattern, msg):
+    return bool(re.fullmatch(pattern, msg))
 
 def check_animal_list(file_path):
-    pass
-
+    with open(file_path) as _input:
+        lines = _input.readlines()
+        
+    m_count, f_count = 0, 0
+    m_pattern = r'^[a-fA-F\d]{8}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{12}_M_[\d]\.[\d]{3}e[\-\+][\d]{2}$'
+    f_pattern = r'^[a-fA-F\d]{8}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{4}\-[a-fA-F\d]{12}_F_[\d]\.[\d]{3}e[\-\+][\d]{2}$'
+    for line in lines:
+        line = line.strip()
+        m_count += match_msg(m_pattern, line)
+        f_count += match_msg(f_pattern, line)
+    print((f_count, m_count))
+    return (f_count, m_count)
 
 if __name__ == '__main__':
     assert check_animal_list('s_animals_sce.txt') == (2, 2)
-    assert check_animal_list('animals_sc_corrupted.txt') == (6, 0)
+    assert check_animal_list('animals_sc_corrupted.txt') == (5, 1)
