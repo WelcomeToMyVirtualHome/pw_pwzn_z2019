3a9d257c791fa685def5993f02a833c692ab2c8a
Jakub Sobolewski <jakupsob@gmail.com>
2019-12-31 00:41:03 +0100

    Task 1

--- lab_10/tasks/task_1.py
+++ lab_10/tasks/task_1.py
@@ -1,9 +1,31 @@
 import requests
+from urllib.parse import urljoin
 
+API_URL = 'https://www.metaweather.com/api/'
 
-def get_cities_woeid(query: str, timeout: float = 5.):
-    pass
-
+def get_cities_woeid(query: str, timeout: float = 5.):    
+    location_url = urljoin(API_URL, 'location/search')
+    data = None
+    try:
+        response = requests.get(location_url, params=dict(query=query), timeout=timeout)
+    except requests.exceptions.Timeout:
+        print(f'Request for: {location_url} took to long!')
+    else:
+        print(f'Status code: {response.status_code}, Took: {response.elapsed}')
+        try: 
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e: 
+            print(e)
+        data = {}
+        try:
+            json_response = response.json()
+        except RuntimeError as e:
+            print(e)
+        finally:
+            for r in json_response:
+                data[r['title']] = r['woeid']
+    finally:
+        return data
 
 if __name__ == '__main__':
     assert get_cities_woeid('Warszawa') == {}
