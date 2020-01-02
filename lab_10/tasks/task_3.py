f551d51689edf2fdd1ead31d50f83500c00175f7
Jakub Sobolewski <jakupsob@gmail.com>
2020-01-02 01:24:36 +0100

    Task 3

--- lab_10/tasks/task_3.py
+++ lab_10/tasks/task_3.py
@@ -1,18 +1,29 @@
 import filecmp
 import pathlib
-from typing import Union
-
 import pandas as pd
+from typing import Union
+from os.path import isfile, join
+from os import listdir
 
-
-API_URL = 'https://www.metaweather.com/api/'
-
-
-def concat_data(
-        path: Union[str, pathlib.Path],
-):
-    pass
-
+def concat_data(path: Union[str, pathlib.Path],):
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    select_cols = ['created', 'min_temp', 'the_temp', 'max_temp', 'air_pressure', 'humidity', 'visibility', 'wind_direction_compass', 'wind_direction', 'wind_speed']
+    _data = None
+    for file_ in files:
+        day = int(file_.split('_')[2].split('.')[0])
+        data = pd.read_csv(pathlib.Path(path) / file_) 
+        data = data[select_cols]
+        data.rename(columns={'the_temp':'temp'}, inplace=True)
+        data.created = pd.to_datetime(data.created)
+        data = data[data.created.dt.day == day]
+        if _data is not None:
+            _data = _data.append(data.copy()) 
+        else:
+            _data = data.copy()
+    _data.sort_values(['created'], inplace=True)
+    _data.created = _data.created.dt.strftime('%Y-%m-%dT%H:%M')
+    with open(str(path) + '.csv', 'w+') as f:
+        f.write(_data.to_csv(index=False))
 
 if __name__ == '__main__':
     concat_data('weather_data/523920_2017_03')
